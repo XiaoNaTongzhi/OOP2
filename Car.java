2e47330890e35acbf06723d80a5ad4382f9cd02b
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-06 20:29:39 +0300

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,76 @@
+public class Car {
+    private String model;
+    private int year;
+    private double price;
+    String color;
+    int power;
+    boolean engineOn;
+
+    Car(String model, int year, double price, String color, int power) {
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.power = power;
+    }
+
+    public void GetInfo() {
+        System.out.println(
+                "Модель: " + model + ", " +
+                        "Год: " + year + ", " +
+                        "Стоимость: " + price + ", " +
+                        "Цвет: " + color + ", " +
+                        "Мощность: " + power);
+    }
+
+    public String GetModel() {
+        return model;
+    }
+
+    public void SetModel(String model) {
+        this.model = model;
+    }
+
+    public int GetYear() {
+        return year;
+    }
+
+    public void SetYear(int year) {
+        this.year = year;
+    }
+
+    public double GetPrice() {
+        return price;
+    }
+
+    public void SetPrice(Double price) {
+        this.price = price;
+    }
+
+    public void powerOn() {
+        if (!engineOn) {
+            this.engineOn = true;
+            System.out.println("Машина завелась.");
+        } else {
+            System.out.println("Машина уже заведена.");
+        }
+    }
+
+    public void powerOff() {
+        if (engineOn) {
+            this.engineOn = false;
+            System.out.println("Двигатель остановлен.");
+        } else {
+            System.out.println("Машина не работает.");
+        }
+    }
+
+    public void driveTo(String place) {
+        if (!engineOn) {
+            System.out.println("Чтобы ехать, нужно сначала завести машину.");
+        } else {
+            System.out.printf("Едем на %s в %s.\n", model, place);
+        }
+    }
+
+}
